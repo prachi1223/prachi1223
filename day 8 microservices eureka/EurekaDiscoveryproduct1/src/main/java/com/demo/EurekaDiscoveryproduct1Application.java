e5185e494c09fbec75dae5e43df0679d08da2700
unknown <prachiawatade23@gmail.com>
2022-05-27 12:10:21 +0000

    Eurekademo

---
+++ day 8 microservices eureka/EurekaDiscoveryproduct1/src/main/java/com/demo/EurekaDiscoveryproduct1Application.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaDiscoveryproduct1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDiscoveryproduct1Application.class, args);
+	}
+
+}
---
+++ day 8 microservices eureka/EurekaDiscoveryproduct1/src/test/java/com/demo/EurekaDiscoveryproduct1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryproduct1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
