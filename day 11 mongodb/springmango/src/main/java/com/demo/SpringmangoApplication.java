8065b10a69420d005ee65a65b3d856260dc24ad5
unknown <prachiawatade23@gmail.com>
2022-06-01 05:26:52 +0000

    springmongodb

---
+++ day 11 mongodb/springmango/src/main/java/com/demo/SpringmangoApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmangoApplication.class, args);
+	}
+
+}
---
+++ day 11 mongodb/springmango/src/main/java/com/demo/serviceimpl/ProdServiceImpl.java
@@ -0,0 +1,25 @@
+package com.demo.serviceimpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.demo.bean.Product;
+import com.demo.repo.MyRepo;
+import com.demo.service.ProdService;
+
+@Service
+public class ProdServiceImpl implements ProdService {
+
+	@Autowired
+	MyRepo myrepo;
+
+	@Override
+	public Product addProduct(Product prod) {
+
+		Product p = myrepo.save(prod);
+
+		return p;
+	}
+
+}
+
