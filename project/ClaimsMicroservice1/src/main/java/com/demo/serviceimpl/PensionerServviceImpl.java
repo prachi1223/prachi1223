242745ee673fbc7fb754dc1df1013c997de26199
unknown <prachiawatade23@gmail.com>
2022-06-13 07:37:29 +0000

    claimdemo

---
+++ project/ClaimsMicroservice1/src/main/java/com/demo/serviceimpl/PensionerServviceImpl.java
@@ -0,0 +1,18 @@
+package com.demo.serviceimpl;
+
+import org.springframework.stereotype.Service;
+
+import com.demo.bean.Pensioner;
+import com.demo.service.PensionerService;
+@Service
+
+public class PensionerServviceImpl implements PensionerService{
+
+	@Override
+	public Pensioner processPension(Pensioner Pensioner) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
